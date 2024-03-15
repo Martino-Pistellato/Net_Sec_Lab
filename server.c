3c92471cae53eb151a020b28ca0db64c282636b9
gap <gapilo.14@outlook.com>
2024-03-15 10:20:35 -0400

    edit filename

--- server.c
+++ server.c
@@ -1,5 +1,7 @@
 // CLIENT
 
+// sorry per i commenti :) -g
+
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
@@ -10,11 +12,11 @@
 void main()
 {
 
-    struct sockaddr_in server;
+    struct sockaddr_in server; // tipo stockaddr_in viene chiamato server
 
-    char buf[1500];
+    char buf[1500]; // crea buffer di 1500 byte.
 
-    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP); // dentro sock ci sarà l'id del socket
     memset((char *)(&server), 0, sizeof(struct sockaddr_in));
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = htonl(INADDR_ANY);
