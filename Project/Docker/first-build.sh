32e30c2413e296bb936eee712328a7f233c3301a
gap <gapilo.14@outlook.com>
2024-07-25 17:24:01 +0200

    Created the network

---
+++ Project/Docker/first-build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# prepare the environment
+docker system prune -a --volumes
+
+# download all images
+docker pull alpine:latest
+docker pull ubuntu:12.04
+docker pull ubuntu:14.04
+docker pull ubuntu:16.04
+docker pull ubuntu:18.04
+docker pull ubuntu:20.04
+docker pull ubuntu:22.04
+
+#build the images and run the containers
+docker compose build
+docker compose up
