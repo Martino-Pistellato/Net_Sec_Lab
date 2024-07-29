d3bc2d14f535300f6dcad52da93492d1f49a572b
Martino-Pistellato <smart.piste@gmail.com>
2024-07-29 20:03:28 +0200

    syn scan update

--- Project/SYN_scan.py
+++ Project/SYN_scan.py
@@ -8,7 +8,7 @@ def backlog_syn_scan(bcklg_size, zombie_ip, zombie_port, target_ip): # for now i
     packets_number = int(bcklg_size*3/4) # we want to fill only 3/4 of the whole backlog in order not to DoS the zombie
     syn_packets_number = int(packets_number/2) # we send half of the total size of packets as SYN packets
     canaries_number = packets_number - syn_packets_number # the other half is filled with canaries
-    ports = np.random.default_rng().choice(np.array([x for x in range(1024, 65536)]), syn_packets_number, replace=False) # we use a list of random ports since we don't actually care if it's open or not
+    ports = np.random.default_rng().choice([x for x in range(1024, 49151)], syn_packets_number, replace=False) # we use a list of random ports since we don't actually care if it's open or not
 
     # eventually, the subnet loop can be placed here 
     syn_packets = scapy.IP(dst=zombie_ip, src=target_ip)/scapy.TCP(dport=zombie_port, sport=ports[0:packets_number], flags="S")
@@ -17,13 +17,13 @@ def backlog_syn_scan(bcklg_size, zombie_ip, zombie_port, target_ip): # for now i
 
     packets = np.random.choice(np.concatenate([syn_packets, canaries]), packets_number, replace=False)  # randomly mixing SYN packets with canaries
     scapy.send(packets, inter=1./5) # sending SYN packets mixed with canaries
-    answered, unanswered = scapy.sr(probes, inter=1./5) # "pinging" the canaries
+    answered, unanswered = scapy.sr(probes, inter=1./5, timeout=1) # "pinging" the canaries, is the timeout ok?
     
-    found = False; # we need to check if target is alive  
-    for packet, response in answered: # basta che ce ne sia uno? E come tempi, devo aspettare qualcosa o posso guardare le risposte subito?
-        if response.flags == "A": 
-            found = True
+    ack = 0.; # we need to check if target is alive  
+    for packet, response in answered: 
+        if response.flags == "A": # how many do we need?
+            ack += 1. 
             print(response.sport, response.flags)
-            break
+    print(f"Ack received: {ack}, Total packets: {packets_number}") # maybe if ack > treshold we consider it alive
     
-    return found
+    return (target_ip, ack/packets_number)
