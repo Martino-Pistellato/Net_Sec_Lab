db1d18d1db4ef68669b4b3587cbc8dfb0ba76c56
gap <gapilo.14@outlook.com>
2024-08-04 22:22:47 +0200

    [WIP] changing routes and routing system

---
+++ Project/Docker/Dockerfile/routers/conf_cont.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Get the IP address of the eth0 interface
+IP_ADDR=$(ip -4 addr show dev eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+
+# Extract the subnet part (first three octets)
+SUBNET=$(echo $IP_ADDR | cut -d '.' -f 1-3)
+
+# Define the new gateway IP using the extracted subnet part
+NEW_GATEWAY="$SUBNET.100"
+
+# Delete the existing default route and add the new one
+ip route del default
+ip route add default via $NEW_GATEWAY dev eth0
