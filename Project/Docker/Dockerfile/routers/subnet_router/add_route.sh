11f63c23dfb7d67abc725f6b20184edce59c3dbe
Vego4801 <thomas.vegoscocco4801@gmail.com>
2024-08-06 13:20:35 +0200

    Reorganized directories + test with FRRouting on main router

--- Project/Docker/Dockerfile/routers/subnet_router/add_route.sh
+++ Project/Docker/Dockerfile/routers/subnet_router/add_route.sh
@@ -7,8 +7,16 @@ IP_ADDR=$(ip -4 addr show dev eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 SUBNET=$(echo $IP_ADDR | cut -d '.' -f 1-3)
 
 # Define the new gateway IP using the extracted subnet part
-NEW_GATEWAY="$SUBNET.100"
+NEW_GATEWAY="$SUBNET.254"
 
 # Delete the existing default route and add the new one
 ip route del default
 ip route add default via $NEW_GATEWAY dev eth0
+
+# Get the subnet from eth1 (e.g., 172.1.0.0/16)
+SUBNET_ETH1=$(ip a show dev eth1 | grep 'inet' | awk '{print $2}' | cut -d '.' -f 1-3)
+
+# Get the subnet from eth0 (e.g., 172.254.0.0/16)
+# SUBNET_ETH0 = $(ip a show dev eth0 | grep 'inet' | awk '{print $2}' | cut -d '.' -f 1-3)
+
+ip route add $SUBNET_ETH1.0/16 dev eth1 via 172.254.0.254
