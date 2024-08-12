9dcfaa6e793b72578d277dd2c19392c7a5108caa
gap <gapilo.14@outlook.com>
2024-08-12 18:37:54 +0200

    [WIP] new net topology. Trouble with gateways
    Co-authored-by: Thomas Vego Scocco <Vego4801@users.noreply.github.com>

--- Project/Docker/Dockerfile/hosts/houseA/conf_cont.sh
+++ Project/Docker/Dockerfile/hosts/houseA/conf_cont.sh
@@ -7,11 +7,8 @@ IP_ADDR=$(ip -4 addr show dev eth0 | grep -Eo 'inet [0-9]+\.[0-9]+\.[0-9]+\.[0-9
 SUBNET=$(echo $IP_ADDR | cut -d '.' -f 1-3)
 
 # Define the new gateway IP using the extracted subnet part
-NEW_GATEWAY="$SUBNET.5"
+NEW_GATEWAY="$SUBNET.61"
 
 # Delete the existing default route and add the new one
 ip route del default
 ip route add default via $NEW_GATEWAY dev eth0
-
-# Enable NAT
-iptables -t nat -A POSTROUTING --out-interface eth0 -j MASQUERADE
