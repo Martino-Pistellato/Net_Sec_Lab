f03ccd0df1d684d1bf0fe7ccb3322dd58c262346
Martino-Pistellato <smart.piste@gmail.com>
2024-08-22 09:49:13 +0200

    sleep() added to "cleanse" the backlog

--- Project/SYN_scan.py
+++ Project/SYN_scan.py
@@ -1,6 +1,7 @@
 import scapy.all as scapy
 import numpy as np
 from scipy.stats import hypergeom
+from time import sleep
 
 # https://scapy.readthedocs.io/en/latest/usage.html
 # 3° pezzo di codice, implementa la tecnica di backlog scan
@@ -9,14 +10,14 @@ def backlog_syn_scan(bcklg_size, zombie_ip, zombie_port, target_subnet): # subne
     packets_number = int(bcklg_size*3/4) # we want to fill only 3/4 of the whole backlog in order not to DoS the zombie
     syn_packets_number = int(packets_number/2) # we send half of the total size of packets as SYN packets
     canaries_number = packets_number - syn_packets_number # the other half is filled with canaries
-    ports = np.random.default_rng().choice([x for x in range(1024, 49151)], syn_packets_number, replace=False) # we use random ports since we don't care if they're open 
+    ports = np.random.default_rng().choice([x for x in range(1024, 49151)], syn_packets_number, replace=False) # we use random ports, doesn't matter if they're open 
     res = []
 
     for i in range(256): #TODO: update based on the network
         trials = []
         target_ip = target_subnet + '.' + i
         syn_packets = scapy.IP(dst=zombie_ip, src=target_ip)/scapy.TCP(dport=zombie_port, sport=ports[0:packets_number], flags="S")
-        canaries = scapy.IP(dst=zombie_ip)/scapy.TCP(dport=zombie_port, sport=ports[0:canaries_number], flags="S", seq=1) #we care about the sequence number because the probes need to have seq-=1
+        canaries = scapy.IP(dst=zombie_ip)/scapy.TCP(dport=zombie_port, sport=ports[0:canaries_number], flags="S", seq=1) # we need seq = 1 because probes need seq-=1
         probes = scapy.IP(dst=zombie_ip)/scapy.TCP(dport=zombie_port, sport=ports[0:canaries_number], flags="S", seq=0) # used to "ping" the canaries
         packets = np.random.choice(np.concatenate([syn_packets, canaries]), packets_number, replace=False)  # randomly mixing SYN packets with canaries
 
@@ -32,6 +33,7 @@ def backlog_syn_scan(bcklg_size, zombie_ip, zombie_port, target_subnet): # subne
             print(f"ACKs received: {ack}, Total canaries: {canaries_number}\n") 
                         
             trials.append({'loss': loss, 'ack': ack, 'k': canaries_number - ack})
+            sleep(packets_number/5) # wait for the backlog to be processed or cleaned
         
         loss1, loss2, loss3 = trials[0]['loss'], trials[1]['loss'], trials[2]['loss']
         ack1, ack2, ack3 = trials[0]['ack'], trials[1]['ack'], trials[2]['ack']
